78a6b738d34718b91c1945f6416d311094d76331
graffitoo <68869505+graffitoo@users.noreply.github.com>
2020-08-10 17:32:14 +0530

    Delete contact.js

--- Contact/contact.js
+++
@@ -1,7 +0,0 @@
-console.log("hellllllo")
-document.querySelector(".zxc").addEventListener("click",()=>{
-    document.querySelector(".zxc").setAttribute("style","background-color:red")
-    document.querySelector(".zxc").innerHTML="chutiya"
-    console.log("qwerty")
-
-})
